319694b2fdd175ddfa4f5a3dee065af539595259
Migram05 <migram05@ucm.es>
2025-05-19 14:53:54 +0200

    Más automatización

---
+++ bin/assets/data/README.txt
@@ -0,0 +1,2 @@
+Esta carpeta es necesaria para copiar los json que se usan en el batch automático
+Este readme está para informar que si la carpeta assets/data no existe, no se podrá completar el proceso. 
---
+++ bin/commands/HeatMap/HeatMapCommandAuto_LVL2.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set searchDir=%~dp0..\..\..\analysis\data\
+for /f %%i in ('dir "%searchDir%" /b/a-d/od/t:c *.json') do set LAST=%%i
+echo The most recently created file is %LAST%
+
+set fileDirectory=%searchDir%%LAST%
+echo The directory is %fileDirectory%
+
+copy "%fileDirectory%" "%~dp0..\..\assets\data\" /Y
+set newDirFile=%~dp0..\..\assets\data\%LAST%
+echo The new directory is %newDirFile%
+ren %newDirFile% HeatMapData.json 
+
+copy "..\Not executing\HeatMapShowAutoLVL2.edencm" "..\" /Y 
+
+setlocal enableextensions disabledelayedexpansion
+
+set readJSONFile=%~dp0..\..\assets\data\HeatMapData.json
+
+set "replace=%readJSONFile%"
+set "search=REPLACE"
+
+set "textFile=..\HeatMapShowAutoLVL2.edencm"
+
+for /f "delims=" %%i in ('type "%textFile%" ^& break ^> "%textFile%" ') do (
+    set "line=%%i"
+    setlocal enabledelayedexpansion
+    >>"%textFile%" echo(!line:%search%=%replace%!
+    endlocal
+)
+
+cd "..\.."
+".\EDEN_Main.exe"
+
+cd ".\commands\"
+
+del ".\HeatMapShowAutoLVL2.edencm"
+
+del "%readJSONFile%"
+
+cd "..\"
